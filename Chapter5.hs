a521b8afba28a7012432ccc22859262f56762f83
David Sanders <dsanders@fusionbox.com>
2012-04-30 13:22:08 -0600

    Some more notes in Chapter 5

--- Chapter5.hs
+++ Chapter5.hs
@@ -11,3 +11,20 @@ maximum'' :: (Ord a) => [a] -> a
 maximum'' [] = error "Can't get maximum of empty list."
 maximum'' [x] = x
 maximum'' (x:xs) = max x (maximum'' xs)
+
+replicate' :: (Num i, Ord i) => i -> a -> [a]
+replicate' n x
+    | n <= 0 = []
+    | otherwise = x:(replicate' (n - 1) x)
+
+take' :: (Num i, Ord i) => i -> [a] -> [a]
+take' _ [] = []
+take' n xs
+    | n <= 0 = []
+    | otherwise = let y:ys = xs in y:(take' (n - 1) ys)
+
+take'' :: (Num i, Ord i) => i -> [a] -> [a]
+take'' _ [] = []
+take'' n _
+    | n <= 0 = []
+take'' n (x:xs) = x:(take'' (n - 1) xs)
