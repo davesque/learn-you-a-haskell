3dd9e86fcba29f79a4718f095bc897841f0ab358
David Sanders <dsanders@fusionbox.com>
2012-04-30 23:47:40 -0600

    Notes from Chapter 5

--- Chapter5.hs
+++ Chapter5.hs
@@ -24,7 +24,40 @@ take' n xs
     | otherwise = let y:ys = xs in y:(take' (n - 1) ys)
 
 take'' :: (Num i, Ord i) => i -> [a] -> [a]
-take'' _ [] = []
 take'' n _
-    | n <= 0 = []
+    | n <= 0    = []
+take'' _ []     = []
 take'' n (x:xs) = x:(take'' (n - 1) xs)
+
+reverse' :: [a] -> [a]
+reverse' [] = []
+reverse' (x:xs) = reverse' xs ++ [x]
+
+repeat' :: a -> [a]
+repeat' x = x:repeat' x
+
+zip' :: [a] -> [b] -> [(a, b)]
+zip' _ [] = []
+zip' [] _ = []
+zip' (x:xs) (y:ys) = (x, y):zip' xs ys
+
+elem' :: (Eq a) => a -> [a] -> Bool
+elem' _ [] = False
+elem' i (x:xs) = if i == x
+                 then True
+                 else elem' i xs
+
+elem'' :: (Eq a) => a -> [a] -> Bool
+elem'' _ [] = False
+elem'' a (x:xs)
+    | a == x    = True
+    | otherwise = elem'' a xs
+
+-- A sorted list is composed of: a sorted list of all elements smaller than or
+-- equal to the head, then the head, then a sorted list of all elements larger
+-- than the head.
+quicksort :: (Ord a) => [a] -> [a]
+quicksort []      = []
+quicksort (x:xs)  = smaller ++ [x] ++ larger
+    where smaller = quicksort [a | a <- xs, a <= x]
+          larger  = quicksort [a | a <- xs, a > x]
