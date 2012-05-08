ae2fc40c452dcc80cb774a9af813d2880d78964a
David Sanders <dsanders@fusionbox.com>
2012-05-07 21:09:47 -0600

    Finish Chapter 6

--- Chapter6.hs
+++ Chapter6.hs
@@ -48,3 +48,59 @@ largestBy3829' = head (filter f [100000,99999..])
 
 sumOfSquares = sum (takeWhile (<10000) (filter odd (map (^2) [1..])))
 sumOfSquares' = sum (takeWhile (<10000) [n^2 | n <- [1..], odd (n^2)])
+
+collatz :: (Integral a) => a -> [a]
+collatz 1 = [1]
+collatz n
+    | even n = n : collatz (n `div` 2)
+    | odd n  = n : collatz (n * 3 + 1)
+
+partialMultipliers :: [Integer -> Integer]
+partialMultipliers = map (*) [0..]
+
+numLongChains :: Int
+numLongChains = length (filter (\xs -> length xs > 15) (map collatz [1..100]))
+
+-- Lambdas: (\args -> expression with args)
+addThree :: (Num a) => a -> a -> a -> a
+addThree x y z = x + y + z
+
+-- Fun gimmick with lambdas to illustrate currying
+addThree' :: (Num a) => a -> a -> a -> a
+addThree' = \x -> \y -> \z -> x + y + z
+
+-- Folds
+sum' :: (Num a) => [a] -> a
+sum' xs = foldl (\a x -> a + x) 0 xs
+
+sum'' :: (Num a) => [a] -> a
+sum'' = foldl (+) 0
+
+product' :: (Num a) => [a] -> a
+product' = foldl (*) 1
+
+factorial :: (Num a, Enum a) => a -> a
+factorial 0 = 1
+factorial x = foldl (*) 1 [1..x]
+
+elem' :: (Eq a) => a -> [a] -> Bool
+elem' x = foldl (\acc i -> acc || x == i) False
+
+-- Function application operator.  $ has lowest precedence.
+testApplication  = sqrt 3 + 4 + 9 == (sqrt 3) + 4 + 9
+testApplication2 = (sqrt $ 3 + 4 + 9) == sqrt (3 + 4 + 9)
+
+-- Make expressions more readable
+testApplication3 = sum (filter (>10) (map (2*) [2..10]))
+testApplication4 = sum $ filter (>10) $ map (2*) [2..10]
+
+-- Use function application operator to call functions
+testApplication5 = map ($ 3) [(4+), (10*), (^2), sqrt]
+
+-- Function composition operator.
+testComposition = map (negate . abs) [1,2,3,4,5,-3,-2,-2]
+
+-- Earlier function written in so-called `point-free` style.  This means the
+-- right-most argument is omitted because of function currying.
+sum''' :: (Num a) => [a] -> a
+sum''' = foldl1 (+)
