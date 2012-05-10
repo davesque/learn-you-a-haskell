b514135b55a24a9d19ece26b1fd8918bad1293c6
David Sanders <dsanders@fusionbox.com>
2012-05-09 18:53:01 -0600

    Start adding tree functions

--- Chapter8.hs
+++ Chapter8.hs
@@ -68,3 +68,21 @@ lockerLookup lockerNumber lockerMap =
 -- Recursive data types can be used.  The built-in list type is one.
 infixr 5 :-:
 data MyList a = Empty | a :-: (MyList a) deriving (Show, Read, Ord, Eq)
+
+-- Infix function that adds two of our lists together
+infixr 5 .++
+(.++) :: MyList a -> MyList a -> MyList a
+Empty .++ ys    = ys
+(x:-:xs) .++ ys = x :-: (xs .++ ys)
+
+data Tree a = EmptyTree | Node a (Tree a) (Tree a) deriving (Show, Read, Eq)
+
+singleton :: a -> Tree a
+singleton x = Node x EmptyTree EmptyTree
+
+treeInsert :: (Ord a) => a -> Tree a -> Tree a
+treeInsert x EmptyTree = singleton x
+treeInsert x (Node a left right)
+    | x == a = (Node a left right)
+    | x < a  = Node a (treeInsert x left) right
+    | x > a  = Node a left (treeInsert x right)
