d63301237d3fdb79d2a4e99a0e68bcf822fa1b7f
David Sanders <dsanders@fusionbox.com>
2012-07-23 23:09:15 -0600

    Add Chapter 15

---
+++ Chapter15.hs
@@ -0,0 +1,56 @@
+data Tree a = Empty | Node a (Tree a) (Tree a) deriving (Show, Read, Eq)
+
+singleton :: a -> Tree a
+singleton x = Node x Empty Empty
+
+treeInsert :: (Ord a) => a -> Tree a -> Tree a
+treeInsert x Empty = singleton x
+treeInsert x (Node a left right)
+    | x == a = (Node a left right)
+    | x < a  = Node a (treeInsert x left) right
+    | x > a  = Node a left (treeInsert x right)
+
+treeElem :: (Ord a) => a -> Tree a -> Bool
+treeElem x Empty = False
+treeElem x (Node a left right)
+    | x == a = True
+    | x < a  = treeElem x left
+    | x > a  = treeElem x right
+
+testTree :: Tree Char
+testTree =
+    Node 'P'
+        (Node 'O'
+            (Node 'L'
+                (Node 'N' Empty Empty)
+                (Node 'T' Empty Empty)
+            )
+            (Node 'Y'
+                (Node 'S' Empty Empty)
+                (Node 'A' Empty Empty)
+            )
+        )
+        (Node 'L'
+            (Node 'W'
+                (Node 'C' Empty Empty)
+                (Node 'R' Empty Empty)
+            )
+            (Node 'A'
+                (Node 'A' Empty Empty)
+                (Node 'C' Empty Empty)
+            )
+        )
+
+changeToP :: Tree Char -> Tree Char
+changeToP (Node x l (Node y (Node _ m n) r)) = (Node x l (Node y (Node 'P' m n) r))
+
+data Direction = L | R deriving (Show)
+type Directions = [Direction]
+
+testDirections :: Directions
+testDirections = [R, L]
+
+changeToP' :: Directions -> Tree Char -> Tree Char
+changeToP' (L:ds) (Node x l r) = Node x (changeToP' ds l) r
+changeToP' (R:ds) (Node x l r) = Node x l (changeToP' ds r)
+changeToP' [] (Node _ l r) = Node 'P' l r
